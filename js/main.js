91229655cf37473f9d3d35bfd1d9be178de05c52
Mads Lentoft <s183919@student.dtu.dk>
2024-09-11 01:33:09 +0200

    BG video playback, working lightbox, simple about me

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function()
 {
     // On first load
-    console.log("Hello world!");
     pageHandler();
 
     // Listeners
@@ -12,10 +11,26 @@ $(document).ready(function()
         $(".page").hide();
         pageHandler();
     });
+    $(".button.play").click(function()
+    {
+        let id = $(this).attr("video-id");
+        playvideo(id);
+    });
+    $("#lightbox").click(function()
+    {
+        $(this).addClass("hidden");
+        removeplayer();
+        $("#bg-video").get(0).play();
+    });
 });
 
 function pageHandler() {
     let hash = window.location.hash;
+    if (hash == "")
+    {
+        // Empty hash, go to first page
+        hash = "#" + $(".page").first().attr("id");
+    }
     $(".page").each(function()
     {
         let id = $(this).attr("id");
@@ -27,11 +42,37 @@ function pageHandler() {
     $(".nav > ul > li > a").each(function()
     {
         $(this).removeClass("selected");
-        
+
         let href = $(this).attr("href");
         if (hash == href)
         {
             $(this).addClass("selected");
         }
     });
+}
+
+function addplayer()
+{
+    $("#lightbox").html('<iframe id="videoplayer" style="border:none;" src="" width="900" height="506"></iframe>');
+}
+
+function removeplayer()
+{
+    $("#videoplayer").remove();
+}
+
+function playvideo(id) {
+    let box = $("#lightbox");
+    let player = $("#videoplayer");
+    let url = "https://drive.google.com/file/d/?/preview";
+    url = url.replace("?", id);
+    // Prepare video
+    addplayer();
+    $("#videoplayer").attr("src", url)
+
+    // Show lightbox
+    box.removeClass("hidden");
+
+    // Pause background
+    $("#bg-video").get(0).pause();
 }
